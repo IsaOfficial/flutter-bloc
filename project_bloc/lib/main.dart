599c9fe83df3046b48db30c09eeab894e99d5524
IsaOfficial <isamaulana2003@gmail.com>
2024-06-04 12:28:27 +0700

    Menggunakan Cubit, Bloc, MultiEvent

---
+++ project_bloc/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'cubit/counter_cubit.dart';
+import 'package:project_bloc/widgets/button.dart';
+import 'bloc/counter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: MultiBlocProvider(
+          providers: [
+            BlocProvider(create: (_) => CounterBloc()),
+            BlocProvider(create: (_) => CounterCubit()),
+          ],
+          child: MyHomePage(title: 'Flutter Demo Home Page'),
+        ));
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  final CounterCubit counterCubit = CounterCubit();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocBuilder<CounterBloc, int>(
+              builder: (context, state) {
+                return Text(
+                  '$state',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton:
+          const IncDecButton(), //jangan lupa untuk mengimpor nanti setelah class ini dibuat
+    );
+  }
+}
---
+++ project_bloc/lib/widgets/button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project_bloc/bloc/counter_bloc.dart';
+
+class IncDecButton extends StatelessWidget {
+  const IncDecButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final counterBloc = BlocProvider.of<CounterBloc>(context);
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        FloatingActionButton(
+          onPressed: () {
+            counterBloc.add(CounterIncremented());
+          },
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ),
+        const SizedBox(height: 10),
+        FloatingActionButton(
+          //onPressed: counterCubit.decrement,
+          onPressed: () {
+            counterBloc.add(CounterDecremented());
+          },
+          tooltip: 'Decrement',
+          child: const Icon(Icons.remove),
+        )
+      ],
+    );
+  }
+}
